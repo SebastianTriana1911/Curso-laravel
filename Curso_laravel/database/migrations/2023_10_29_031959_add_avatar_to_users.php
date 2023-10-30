0077cb45cd42f30fbe292b752d4083d9b9ae047e
Sebastian <sebastiantriana1911@gmail.com>
2023-10-30 10:26:23 -0500

    Registros con Seeder

--- Curso_laravel/database/migrations/2023_10_29_031959_add_avatar_to_users.php
+++ Curso_laravel/database/migrations/2023_10_29_031959_add_avatar_to_users.php
@@ -1,37 +1,36 @@
 <?php
-// M
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        // Metodo table - se utiliza cuando se desea actualizar una 
-        // tabla que ya existe
-        Schema::table('users', function (Blueprint $table) {
-            // Se asigna el nuevo campo que se le agregara a la tabla.
-            // Si deseamos crear un nuevo campo a una tabla que ya
-            // tenga registros y no deseamos que nos genere error es
-            // importante asignarle al campo el metodo nullable
-            $table->string("avatar")->nullable()->after("email");
-            // El metodo after nos ayudara a insertar el nuevo campo 
-            // adelante del campo que le asignemos como parametro
-        });
-    }
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      */
+//     public function up(): void
+//     {
+//         // Metodo table - se utiliza cuando se desea actualizar una 
+//         // tabla que ya existe
+//         Schema::table('users', function (Blueprint $table) {
+//             // Se asigna el nuevo campo que se le agregara a la tabla.
+//             // Si deseamos crear un nuevo campo a una tabla que ya
+//             // tenga registros y no deseamos que nos genere error es
+//             // importante asignarle al campo el metodo nullable
+//             $table->string("avatar")->nullable()->after("email");
+//             // El metodo after nos ayudara a insertar el nuevo campo 
+//             // adelante del campo que le asignemos como parametro
+//         });
+//     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn("avatar");
-        });
-    }
-};
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function down(): void
+//     {
+//         Schema::table('users', function (Blueprint $table) {
+//             $table->dropColumn("avatar");
+//         });
+//     }
+// };
--- Curso_laravel/database/seeders/DatabaseSeeder.php
+++ Curso_laravel/database/seeders/DatabaseSeeder.php
@@ -1,8 +1,18 @@
 <?php
 
+// Los Seeder funcionan de la misma manera que creando registros desde los
+// modelos con el comando php artisan make:tinker, pues en los seeder es
+// necesario instanciar un objeto de la clase que representa la tabla en 
+// nuestra base de datos y de ahi se le asignan las columnas con sus respectivos
+// datos
+
+// Dicho objeto con los registros ya creados dse suben a la base de datos con
+// el comando php artisan db:seed
+
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Curso;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +28,29 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Instancia de una clase (Tabla) Curso
+        $curso = new Curso();
+
+        // Objeto instanciado con los registros de los diferentes campos
+        // de la tabla
+        $curso -> nombre = "Laravel";
+        $curso -> descripcion = "El mejor framework de PHP";
+        $curso -> categoria = "Desarrollo web";
+
+        // Objeto salvado para con el comando db:seed subir dichos registros
+        // que contiene a la base de datos
+        $curso -> save();
+
+        // ------------------------  NUEVO REGISTRO ----------------------------
+
+        $curso2 = new Curso();
+
+        $curso2 -> nombre = "PHP";
+        $curso2 -> descripcion = "El mejor lenguaje de programacion";
+        $curso2 -> categoria = "Desarrollo web";
+
+        $curso2 -> save();
+        
     }
 }
